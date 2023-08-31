5aac31eb5e4643100349afcf34ee31d012b0164b
Patrick Louis <patricklouis@Patricks-MacBook-Pro.local>
2023-08-31 16:58:27 -0400

    Create User Model, User Manager, and SuperUser

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Setting User Model for Authentication
+AUTH_USER_MODEL = 'core.User'
