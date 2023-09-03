0050b9f006136da3bd0a2d8d219a23f9cc2f9a78
Patrick Louis <patricklouis@Patricks-MacBook-Pro.local>
2023-09-03 18:06:20 -0400

    Build Ingredients API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,10 +4,10 @@ Tests for models.
 from decimal import Decimal
 
 from django.test import TestCase
-# Provided by Django to get the default user model
 from django.contrib.auth import get_user_model
 
 from core import models
+
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create a return a new user."""
@@ -29,7 +29,6 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
-
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
@@ -41,7 +40,6 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
-
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
@@ -57,7 +55,6 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-
 
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
@@ -81,3 +78,13 @@ class ModelTests(TestCase):
         tag = models.Tag.objects.create(user=user, name='Tag1')
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient is successful."""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
